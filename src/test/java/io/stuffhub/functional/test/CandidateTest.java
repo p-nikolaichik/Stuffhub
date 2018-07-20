a7bf3922f23a18e9dd356c73f69b780e5d05458f
pavel <p.nikolaichik@mail.ru>
2018-07-20 18:18:25 +0300

    Fixed directory to download final

--- src/test/java/io/stuffhub/functional/test/CandidateTest.java
+++ src/test/java/io/stuffhub/functional/test/CandidateTest.java
@@ -105,12 +105,12 @@ public class CandidateTest extends BaseTest{
 
         long start = System.currentTimeMillis();
         long currentTime = 0;
-        while (currentTime <= 10000) {
+        while (true) {
             array = file.listFiles();
             for (int i = 0; i < array.length; i++) {
                 isExistFile = array[i].getName().contains(name);
                 currentTime = System.currentTimeMillis() - start;
-                if (isExistFile == true) {
+                if (isExistFile == true || currentTime > 10000) {
                     break;
                 }
             }
